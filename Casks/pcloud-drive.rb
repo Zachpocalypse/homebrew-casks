01687c4619d6a1b8c39b1ac7d0dc1058ed4169c6
Zachary Trant <ztrant@gmail.com>
2020-08-13 18:45:53 -0500

    Added cask
    Added cask

---
+++ Casks/pcloud-drive.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+cask 'pcloud-drive' do
+  version '3.7.9'
+  sha256 '05e6ed62185d689688894096e0b9c32ee9256b77d4f4e1503d60dbfec1b5c593'
+
+  url do
+    require 'net/http'
+    require 'json'
+    api = 'https://api.pcloud.com/'
+    code = 'XZxCdakZRh9qTwDrTU57A4KXw5E2CVMnLrDV'
+    uri = URI(api + 'getpublinkdownload?code=' + code)
+    response = Net::HTTP.get(uri)
+    data = JSON.parse(response)
+    data['hosts'][0] + data['path']
+  end
+  name 'pCloud Drive'
+  homepage 'https://www.pcloud.com/'
+
+  depends_on cask: 'osxfuse'
+
+  pkg 'pCloud Drive 3.9.4.pkg'
+
+  uninstall quit:    'com.pcloud.pcloud.macos',
+            pkgutil: 'com.mobileinno.pkg.pCloudDrive'
+end
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-homebrew-pcloud
+# Zachpocalypse Pcloud
+
+## How do I install these formulae?
+`brew install zachpocalypse/pcloud/<formula>`
+
+Or `brew tap zachpocalypse/pcloud` and then `brew install <formula>`.
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
